466d4ca207cb0ba24fe87ac2ebffe39dc757e13f
Dominik Klein <klein@klein-mobil.fkie.fraunhofer.de>
2021-03-16 10:56:15 +0100

    IcBarcode CMake fixed

--- CMakeModules/FindIcBarcode.cmake
+++ CMakeModules/FindIcBarcode.cmake
@@ -2,18 +2,18 @@
 find_package(PkgConfig QUIET)
 
 if( PKG_CONFIG_FOUND )
-  pkg_check_modules( ic_barcode ic_barcode )
+  pkg_check_modules( IcBarcode ic_barcode )
 endif()
 
-if( NOT ic_barcode_FOUND )
+if( NOT IcBarcode_FOUND )
   message("ic_barcode could not be found by pkg-config. Trying to manually find ic_barcode.")
-  find_path(ic_barcode_INCLUDE_DIRS ic_barcode.h
+  find_path(IcBarcode_INCLUDE_DIRS ic_barcode.h
     PATHS
     "$ENV{IC_BARCODE_INCLUDE_PATH}"
     /usr/local/include/
     /usr/include/
   )
-  find_library(IC_BARCODE_LIBRARIES aravis-0.6
+  find_library(IcBarcode_LIBRARIES ic_barcode
     PATHS
     "$ENV{IC_BARCODE_LIBRARY}"
     /usr/local/lib
@@ -22,8 +22,8 @@ if( NOT ic_barcode_FOUND )
   )
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args( IC_BARCODE DEFAULT_MSG
-    IC_BARCODE_INCLUDE_DIRS
-    IC_BARCODE_LIBRARIES
+    IcBarcode_INCLUDE_DIRS
+    IcBarcode_LIBRARIES
   )
 endif()
     
